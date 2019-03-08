11cde9bfa26787ebe33d990915fbd736a3778a51
michaelheyman <mitch.heyman@gmail.com>
2019-03-07 19:03:51 -0800

    Add Types module

---
+++ src/Types.hs
@@ -0,0 +1,58 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Types where
+
+import           Control.Exception.Base (evaluate)
+
+import           Control.Lens           (view, (^.), _1, _2, _3, _4)
+import           Data.Aeson             (ToJSON (..), object, (.=))
+import           Data.Text.Internal     (Text)
+import           Database.Schema
+import           GHC.Int                (Int64)
+
+type ClassQueryResult =
+  (ClassOffering
+  , Course
+  , Instructor
+  , Term)
+
+data Class = Class
+  { classId    :: Int64
+  , course     :: Course
+  , instructor :: Maybe Instructor
+  , term       :: Term
+  , credits    :: Int64
+  , days       :: Text
+  , time       :: Text
+  , crn        :: Int64
+  , timestamp  :: Text -- UTCTime
+  } deriving (Show)
+
+instance ToJSON Class where
+  toJSON (Class classId course instructor term credits days time crn timestamp) =
+    object [ "id"         .= classId
+           , "course"     .= course
+           , "instructor" .= instructor
+           , "term"       .= term
+           , "credits"    .= credits
+           , "days"       .= days
+           , "time"       .= time
+           , "crn"        .= crn
+           , "timestamp"  .= timestamp ]
+
+toClass :: ClassQueryResult -> Class
+toClass c = Class
+  (_classOfferingId classOffering)
+  course
+  (Just instructor)
+  term
+  (_classOfferingCredits classOffering)
+  (_classOfferingDays classOffering)
+  (_classOfferingTime classOffering)
+  (_classOfferingCrn classOffering)
+  (_classOfferingTimestamp classOffering)
+ where
+  classOffering = view _1 c
+  course        = view _2 c
+  instructor    = view _3 c
+  term          = view _4 c
